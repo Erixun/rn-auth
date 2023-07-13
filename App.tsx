f89a16715b77e0f6da324e9ec495ffabe7eba6bd
Erik Sundberg <erik.sundberg@unicus.com>
2023-07-13 18:28:58 +0200

    modularize, refactor & fix colors & styling

--- App.tsx
+++ App.tsx
@@ -1,80 +1,15 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-
-import LoginScreen from './screens/LoginScreen';
-import SignupScreen from './screens/SignupScreen';
-import WelcomeScreen from './screens/WelcomeScreen';
-import { Colors } from './constants/styles';
 import { AuthContextProvider } from './store/authContext';
-
-const LOGIN = 'Login';
-const SIGNUP = 'Signup';
-const WELCOME = 'Welcome';
-
-export const Screen: ScreenKeys = {
-  LOGIN,
-  SIGNUP,
-  WELCOME,
-};
-
-export type ScreenKeys = { [x: string]: screen };
-export type screen = keyof RootStackParamList;
-
-export type RootStackParamList = {
-  [LOGIN]: undefined;
-  [SIGNUP]: undefined;
-  [WELCOME]: undefined;
-};
-
-export const Stack = createNativeStackNavigator<RootStackParamList>();
-
-function AuthStack() {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: Colors.primary500 },
-        headerTintColor: 'white',
-        contentStyle: { backgroundColor: Colors.primary100 },
-      }}
-    >
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Signup" component={SignupScreen} />
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
-    </Stack.Navigator>
-  );
-}
-
-function AuthenticatedStack() {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: Colors.primary500 },
-        headerTintColor: 'white',
-        contentStyle: { backgroundColor: Colors.primary100 },
-      }}
-    >
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
-    </Stack.Navigator>
-  );
-}
-
-function Navigation() {
-  return (
-    <AuthContextProvider>
-      <NavigationContainer>
-        <AuthStack />
-      </NavigationContainer>
-    </AuthContextProvider>
-  );
-}
+import { Navigation } from './navigators/Navigation';
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
 
-      <Navigation />
+      <AuthContextProvider>
+        <Navigation />
+      </AuthContextProvider>
     </>
   );
 }
